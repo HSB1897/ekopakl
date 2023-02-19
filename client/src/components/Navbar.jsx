94bec1d46c5c2488058c3686cc7422e674c60ecd
JaafarHawli <jaafarhawle@gmail.com>
2023-02-20 00:30:02 +0200

    client: updated nav bar elements hover effect

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,13 +20,13 @@ const Navbar = (props) => {
   return (
     <div className={openNav ? 'flex flex-col md:w-[80%] w-[300px] md:flex-row py-4 px-24 justify-between items-center absolute z-50 ' : 'min-h-[70px] items-center w-screen flex flex-col md:w-[80%] md:flex-row py-4 px-24 justify-between align-middle absolute z-50'}>
       <GiHamburgerMenu className='absolute top-6 left-6 md:hidden text-white text-[24px]' onClick={() => setOpenNav(!openNav)} />
-      <img src={logo} alt="" className={openNav ? 'w-[50px] mb-4 md:mb-0 select-none' : ' select-none hidden md:block md:w-[50px] md:mb-0'} onClick={() => { navigateToHomePage(); props.setSection('')}} />
+      <img src={logo} alt="" className={openNav ? 'w-[60px] mb-4 md:mb-0 select-none' : ' select-none hidden md:block md:w-[60px] md:mb-0'} onClick={() => { navigateToHomePage(); props.setSection('')}} />
       <div className={openNav ? 'flex flex-col md:flex-row gap-6 select-none' : 'hidden md:flex md:flex-row md:gap-6 md:select-none'}>
-          <h1 className='text-[20px] font-semibold text-navy cursor-pointer neon' onClick={() => { navigateToHomePage(); props.setSection('services')}}>Services</h1>
-          <h1 className='text-[20px] font-semibold text-navy cursor-pointer neon' onClick={() => { navigateToHomePage(); props.setSection('ourTeam')}}>Our Team</h1>
-          <h1 className='text-[20px] font-semibold text-navy cursor-pointer neon' onClick={() => { navigateToHomePage(); props.setSection('partners')}}>Partners</h1>
-          <h1 className='text-[20px] font-semibold text-navy cursor-pointer neon' onClick={() => { navigateToHomePage(); props.setSection('contactUs')}}>Contact Us</h1>
-          <h1 className='text-[20px] font-semibold text-navy cursor-pointer neon' onClick={() => { navigateToAboutUsPage(); props.setSection('')}}>About Us</h1>
+          <h1 className='text-[20px] font-semibold text-navy cursor-pointer relative nav-item' onClick={() => { navigateToHomePage(); props.setSection('services')}}>Services</h1>
+          <h1 className='text-[20px] font-semibold text-navy cursor-pointer relative nav-item' onClick={() => { navigateToHomePage(); props.setSection('ourTeam')}}>About</h1>
+          <h1 className='text-[20px] font-semibold text-navy cursor-pointer relative nav-item' onClick={() => { navigateToHomePage(); props.setSection('partners')}}>Team</h1>
+          <h1 className='text-[20px] font-semibold text-navy cursor-pointer relative nav-item' onClick={() => { navigateToHomePage(); props.setSection('contactUs')}}>Partners</h1>
+          <h1 className='text-[20px] font-semibold text-navy cursor-pointer relative nav-item' onClick={() => { navigateToAboutUsPage(); props.setSection('')}}>Contact</h1>
       </div>
     </div>
   );
--- client/src/pages/Home/About.jsx
+++ client/src/pages/Home/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <div className='w-1/2'> </div>
       <div className='flex flex-col gap-10 w-1/2'>
           <Headline className='text-navy'>About Us</Headline>
-          <p className='text-navy text-[22px] text-justify'>EkoPak Sal is managed by a team of experienced and dedicated professionals who are committed to ensuring the success of the company. They bring a wealth of knowledge, experience, and expertise to their roles and are dedicated to delivering high-quality work that meets the needs of the company and its clients</p>
+          <p className='text-navy text-[22px] text-justify'>Welcome to EkoPak Sal, a leading trading company. With a presence in Nigeria, Senegal, and Ghana, we have established a reputation as a reliable and trusted provider of high-quality products. Our company is committed to sourcing the best products from around the world, including Austria, Sweden, Brazil, China, and India, and delivering them to factories in West Africa.</p>
           <Button className=' self-end hover:bg-navy hover:text-secondary duration-200' arrowHoverColor={"secondary"}>Read More</Button>
       </div>
     </Container>
