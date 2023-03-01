fdddfb954e53e9d0a8b11de5e5459c5282c3b842
JaafarHawli <jaafarhawle@gmail.com>
2023-03-01 22:09:36 +0200

    client: fixed main page team section responsiveness

--- client/src/components/Headline.jsx
+++ client/src/components/Headline.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Headline = (props) => {
   return (
-    <h1 className={`text-[52px] font-bold ${props.className}`}>
+    <h1 className={`lg:text-[52px] text-[42px] font-bold ${props.className}`}>
         {props.children}
     </h1>
   )
